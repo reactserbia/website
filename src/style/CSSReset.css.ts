302f88a0e6332eeb638264880110ee2c0bbcd0a6
Mirko Basic <bejzik8@gmail.com>
2023-11-29 00:29:46 +0100

    Added CSS Reset. 🍏

---
+++ src/style/CSSReset.css.ts
@@ -0,0 +1,30 @@
+import { globalStyle } from '@vanilla-extract/css'
+
+import { theme } from './theme.css'
+
+globalStyle('*', {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box'
+})
+
+globalStyle('body', {
+    width: '100vw',
+    minHeight: '100vh',
+    position: 'relative',
+    background: theme.colors.no1appBackground,
+    overflowX: 'hidden'
+})
+
+globalStyle(':root', {
+    lineHeight: 1.5,
+    fontFamily:
+        '-apple-system, BlinkMacSystemFont, Segoe UI, Noto Sans, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji',
+    fontWeight: 400,
+    fontSynthesis: 'none',
+    textRendering: 'optimizeLegibility',
+    WebkitFontSmoothing: 'antialiased',
+    MozOsxFontSmoothing: 'grayscale',
+    WebkitTextSizeAdjust: '100%',
+    margin: '0 auto'
+})
