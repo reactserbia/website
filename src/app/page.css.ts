ccf820b4149e9828fa656f8f8d80efcaf3d2f914
Mirko Basic <bejzik8@gmail.com>
2023-12-04 20:44:55 +0100

    Added info section. ℹ️

--- src/app/page.css.ts
+++ src/app/page.css.ts
@@ -20,6 +20,13 @@ export const bannerContainer = style({
     padding: '64px 0',
     borderBottom: `2px solid ${theme.colors.tertiary.no12}`,
     backgroundColor: theme.colors.tertiary.no02,
-    backgroundImage: `linear-gradient(90deg, ${theme.colors.tertiary.no01} calc(100% - 1px), ${theme.colors.tertiary.no05} 0)`,
-    backgroundSize: `calc((100% - 1 * 1px) / 200 + 1px) 100%`
+    backgroundImage: `linear-gradient(180deg, ${theme.colors.tertiary.no01} calc(100% - 1px), ${theme.colors.tertiary.no05} 0)`,
+    backgroundSize: `100% calc((100% - 1 * 1px) / 50 + 1px)`
+})
+
+export const infoContainer = style({
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: '64px 0'
 })
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 import { Heading } from '@/components/atoms'
 
-import { container, content, bannerContainer } from './page.css'
+import { container, content, bannerContainer, infoContainer } from './page.css'
 import {
     COLOR_VARIANTS,
     LAYERS_VARIANTS,
@@ -23,67 +23,17 @@ export default function Home() {
                     />
                 </div>
             </div>
-            <div className={content}>
-                <Heading sizeVariant={SIZE_VARIANTS.LARGE}>WHO ARE WE?</Heading>
-                <hr />
-                <Heading sizeVariant={SIZE_VARIANTS.MEDIUM}>
-                    WHO ARE WE?
-                </Heading>
-                <hr />
-                <Heading>WHO ARE WE?</Heading>
-                <hr />
-                <Heading colorVariant={COLOR_VARIANTS.SECONDARY}>
-                    WHO ARE WE?
-                </Heading>
-                <hr />
-                <Heading colorVariant={COLOR_VARIANTS.TERTIARY}>
-                    WHO ARE WE?
-                </Heading>
-                <hr />
-                <Heading shadeVariant={SHADE_VARIANTS.DARK}>
-                    WHO ARE WE?
-                </Heading>
-                <hr />
-                <Heading
-                    colorVariant={COLOR_VARIANTS.SECONDARY}
-                    shadeVariant={SHADE_VARIANTS.DARK}
-                >
-                    WHO ARE WE?
-                </Heading>
-                <hr />
-                <Heading
-                    colorVariant={COLOR_VARIANTS.TERTIARY}
-                    shadeVariant={SHADE_VARIANTS.DARK}
-                >
-                    WHO ARE WE?
-                </Heading>
-                <hr />
-                <Heading
-                    shadeVariant={SHADE_VARIANTS.DARK}
-                    layers={LAYERS_VARIANTS.THREE}
-                    sizeVariant={SIZE_VARIANTS.LARGE}
-                >
-                    WHO ARE WE?
-                </Heading>
-                <hr />
-                <Heading
-                    colorVariant={COLOR_VARIANTS.SECONDARY}
-                    shadeVariant={SHADE_VARIANTS.DARK}
-                    layers={LAYERS_VARIANTS.THREE}
-                    sizeVariant={SIZE_VARIANTS.LARGE}
-                >
-                    WHO ARE WE?
-                </Heading>
-                <hr />
-                <Heading
-                    colorVariant={COLOR_VARIANTS.TERTIARY}
-                    shadeVariant={SHADE_VARIANTS.DARK}
-                    layers={LAYERS_VARIANTS.THREE}
-                    sizeVariant={SIZE_VARIANTS.LARGE}
-                >
-                    WHO ARE WE?
-                </Heading>
-                <hr />
+            <div className={infoContainer}>
+                <div className={content}>
+                    <Heading
+                        colorVariant={COLOR_VARIANTS.TERTIARY}
+                        shadeVariant={SHADE_VARIANTS.DARK}
+                        layers={LAYERS_VARIANTS.THREE}
+                        sizeVariant={SIZE_VARIANTS.LARGE}
+                    >
+                        WELCOME!
+                    </Heading>
+                </div>
             </div>
         </main>
     )
