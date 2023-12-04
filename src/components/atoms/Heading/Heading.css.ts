291e740b701109c14bc57cce8a363109a58f554d
Mirko Basic <bejzik8@gmail.com>
2023-12-04 02:29:13 +0100

    Updated Heading component. 😶

--- src/components/atoms/Heading/Heading.css.ts
+++ src/components/atoms/Heading/Heading.css.ts
@@ -12,6 +12,33 @@ export const base = style({
     margin: 0
 })
 
+export const sizeVariants = styleVariants({
+    [SIZE_VARIANTS.SMALL]: {
+        fontSize: '2rem'
+    },
+    [SIZE_VARIANTS.MEDIUM]: {
+        fontSize: '3rem'
+    },
+    [SIZE_VARIANTS.LARGE]: {
+        fontSize: '4rem'
+    }
+})
+
+export const textColorVariants = styleVariants({
+    [COLOR_VARIANTS.PRIMARY]: {
+        color: theme.colors.primary.no03,
+        WebkitTextStrokeColor: theme.colors.primary.no12
+    },
+    [COLOR_VARIANTS.SECONDARY]: {
+        color: theme.colors.secondary.no03,
+        WebkitTextStrokeColor: theme.colors.secondary.no12
+    },
+    [COLOR_VARIANTS.TERTIARY]: {
+        color: theme.colors.tertiary.no03,
+        WebkitTextStrokeColor: theme.colors.tertiary.no12
+    }
+})
+
 export const spanBase = style({
     position: 'absolute',
     whiteSpace: 'nowrap',
@@ -75,31 +102,4 @@ export const spanTwoVariants = styleVariants({
             top: 10
         }
     ]
-})
-
-export const textColorVariants = styleVariants({
-    [COLOR_VARIANTS.PRIMARY]: {
-        color: theme.colors.primary.no03,
-        WebkitTextStrokeColor: theme.colors.primary.no12
-    },
-    [COLOR_VARIANTS.SECONDARY]: {
-        color: theme.colors.secondary.no03,
-        WebkitTextStrokeColor: theme.colors.secondary.no12
-    },
-    [COLOR_VARIANTS.TERTIARY]: {
-        color: theme.colors.tertiary.no03,
-        WebkitTextStrokeColor: theme.colors.tertiary.no12
-    }
-})
-
-export const sizeVariants = styleVariants({
-    [SIZE_VARIANTS.SMALL]: {
-        fontSize: '2rem'
-    },
-    [SIZE_VARIANTS.MEDIUM]: {
-        fontSize: '3rem'
-    },
-    [SIZE_VARIANTS.LARGE]: {
-        fontSize: '4rem'
-    }
 })
