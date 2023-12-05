3e11f1af29506eb366fda2eb5a4308219059a46a
Mirko Basic <bejzik8@gmail.com>
2023-12-05 17:26:11 +0100

    Updated NoteEntry component. 📝

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,10 +83,14 @@ export default function Home() {
                         BILLBOARD
                     </Heading>
                     <Billboard>
-                        <NoteEntry />
-                        <NoteEntry />
-                        <NoteEntry />
-                        <NoteEntry />
+                        <NoteEntry
+                            heading='What is React?'
+                            content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+                        />
+                        <NoteEntry
+                            heading='What is React?'
+                            content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+                        />
                     </Billboard>
                 </div>
             </div>
