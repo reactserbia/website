92b152e5274d672cfe197a98c9629dbb2882d7b1
Mirko Basic <bejzik8@gmail.com>
2023-12-05 20:35:02 +0100

    Updated NavigationButton component. 🐭

--- src/components/atoms/NavigationButton/NavigationButton.css.ts
+++ src/components/atoms/NavigationButton/NavigationButton.css.ts
@@ -2,6 +2,12 @@ import { theme } from '@/style/theme.css'
 import { style } from '@vanilla-extract/css'
 
 export const buttonBase = style({
+    position: 'relative',
+    border: 'none'
+})
+
+export const buttonSpan = style({
+    display: 'inline-block',
     padding: '0.5rem',
     border: `1px solid ${theme.colors.primary.no11}`,
     borderRadius: '0.25rem',
@@ -10,5 +16,3 @@ export const buttonBase = style({
     color: theme.colors.primary.no12,
     backgroundColor: theme.colors.primary.no03
 })
-
-export const buttonSpan = style({})
--- src/components/atoms/NavigationButton/NavigationButton.tsx
+++ src/components/atoms/NavigationButton/NavigationButton.tsx
@@ -1,11 +1,14 @@
 import { ReactNode } from 'react'
-import { buttonBase } from './NavigationButton.css'
-import { spanBase } from '../Heading/Heading.css'
+import { buttonBase, buttonSpan } from './NavigationButton.css'
 
 type NavigationButtonProps = {
     children: ReactNode
 }
 
 export function NavigationButton({ children }: NavigationButtonProps) {
-    return <button className={buttonBase}>{children}</button>
+    return (
+        <button className={buttonBase}>
+            <span className={buttonSpan}>{children}</span>
+        </button>
+    )
 }
