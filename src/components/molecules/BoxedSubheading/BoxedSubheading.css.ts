ee5c14c05ff2c634e01cf33100c229b1d3b06ef0
Mirko Basic <bejzik8@gmail.com>
2023-12-06 17:32:24 +0100

    Updated BoxedSubheading component. 🦔

--- src/components/molecules/BoxedSubheading/BoxedSubheading.css.ts
+++ src/components/molecules/BoxedSubheading/BoxedSubheading.css.ts
@@ -1,6 +1,7 @@
 import { style } from '@vanilla-extract/css'
 
 export const container = style({
+    padding: '0.25rem 1rem',
     border: '1px solid black',
     borderRadius: 999
 })
--- src/components/molecules/BoxedSubheading/BoxedSubheading.tsx
+++ src/components/molecules/BoxedSubheading/BoxedSubheading.tsx
@@ -2,15 +2,20 @@ import { ReactNode } from 'react'
 
 import { Subheading } from '@/components/atoms'
 import { container } from './BoxedSubheading.css'
+import { SIZE_VARIANTS } from '@/constants'
 
 type BoxedSubheadingProps = {
     children: ReactNode
+    sizeVariant?: SIZE_VARIANTS
 }
 
-export function BoxedSubheading({ children }: BoxedSubheadingProps) {
+export function BoxedSubheading({
+    children,
+    sizeVariant = SIZE_VARIANTS.SMALL
+}: BoxedSubheadingProps) {
     return (
         <div className={container}>
-            <Subheading>{children}</Subheading>
+            <Subheading sizeVariant={sizeVariant}>{children}</Subheading>
         </div>
     )
 }
