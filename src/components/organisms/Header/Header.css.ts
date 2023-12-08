dac7f5971d57ecb48719139c4e514c313e655364
Mirko Basic <bejzik8@gmail.com>
2023-12-08 16:38:47 +0100

    Updated Header component. 💆

--- src/components/organisms/Header/Header.css.ts
+++ src/components/organisms/Header/Header.css.ts
@@ -3,43 +3,12 @@ import { style } from '@vanilla-extract/css'
 import { theme } from '@/style/theme.css'
 
 export const header = style({
-    height: theme.dimensions.headerHeight,
-    display: 'flex',
-    alignItems: 'center',
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    zIndex: 5,
-    borderBottom: `2px solid ${theme.colors.secondary.no12}`,
-    backgroundColor: theme.colors.secondary.no02,
-    boxShadow: `0 6px 0 ${theme.colors.secondary.no04}`
-})
-
-export const headerContent = style({
-    width: '100%',
     maxWidth: theme.dimensions.contentMaxWidth,
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
+    padding: '1rem 1.5rem',
+    border: `2px solid ${theme.colors.black}`,
+    backgroundColor: theme.colors.tertiary.no02,
     margin: '0 auto'
-})
-
-export const logoContainer = style({
-    position: 'absolute',
-    bottom: 0,
-    left: '50%',
-    transform: 'translate(-50%, 50%)'
-})
-
-export const logo = style({
-    position: 'absolute',
-    bottom: '50%',
-    left: '50%',
-    transform: 'translateX(-50%)'
-})
-
-export const navigation = style({
-    display: 'flex',
-    gap: '0.5rem'
 })
--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,44 +1,13 @@
-import Image from 'next/image'
-
 import { COLOR_VARIANTS } from '@/constants'
 import { Heading, NavigationButton } from '@/components/atoms'
-import {
-    header,
-    headerContent,
-    logo,
-    logoContainer,
-    navigation
-} from './Header.css'
+import { header } from './Header.css'
 
 export function Header() {
     return (
         <header className={header}>
-            <div className={headerContent}>
-                <div>
-                    <Heading colorVariant={COLOR_VARIANTS.TERTIARY}>
-                        REACT
-                    </Heading>
-                    <Heading>SERBIA</Heading>
-                </div>
-                <div className={logoContainer}>
-                    <Image
-                        width={135}
-                        height={85}
-                        src='/images/logo-cloud.svg'
-                        alt='logo-cloud'
-                    />
-                    <Image
-                        width={42}
-                        height={42}
-                        src='/images/logo-rs.svg'
-                        alt='logo-cloud'
-                        className={logo}
-                    />
-                </div>
-                <nav className={navigation}>
-                    <NavigationButton>Home</NavigationButton>
-                    <NavigationButton>About</NavigationButton>
-                </nav>
+            <div>
+                <Heading colorVariant={COLOR_VARIANTS.TERTIARY}>REACT</Heading>
+                <Heading>SERBIA</Heading>
             </div>
         </header>
     )
