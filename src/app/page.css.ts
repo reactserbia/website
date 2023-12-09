0c9dead16ad10e15e8ce4b057465f1e6be9e5ae7
Mirko Basic <bejzik8@gmail.com>
2023-12-09 03:56:45 +0100

    Removed unused styles. 🐓

--- src/app/page.css.ts
+++ src/app/page.css.ts
@@ -2,18 +2,6 @@ import { keyframes, style } from '@vanilla-extract/css'
 
 import { theme } from '@/style/theme.css'
 
-export const content = style({
-    width: '100%',
-    maxWidth: theme.dimensions.contentMaxWidth,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'column',
-    gap: '1rem',
-    textAlign: 'center',
-    margin: '0 auto'
-})
-
 export const bannerContainer = style({
     width: '100%',
     display: 'flex',
@@ -24,13 +12,6 @@ export const bannerContainer = style({
     padding: theme.dimensions.sectionPadding,
     border: theme.border,
     background: `linear-gradient(180deg, ${theme.colors.tertiary.no01} 0%, ${theme.colors.tertiary.no03} 100%)`
-})
-
-export const midSectionLogoContainer = style({
-    position: 'absolute',
-    bottom: 0,
-    left: '50%',
-    transform: 'translate(-50%, 50%)'
 })
 
 export const infoContainer = style({
@@ -44,29 +25,6 @@ export const infoContainer = style({
     background: `linear-gradient(180deg, ${theme.colors.tertiary.no03} 0%, rgba(105, 217, 193, 0.00) 100%)`
 })
 
-export const infoSectionsContainer = style({
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: '2rem'
-})
-
-const moving = keyframes({
-    from: {
-        left: -500
-    },
-    to: {
-        left: '100vw'
-    }
-})
-
-export const cloud = style({
-    position: 'absolute',
-    top: '120px',
-    zIndex: 0,
-    animation: `${moving} 50s linear infinite`
-})
-
 export const billboardContainer = style({
     display: 'flex',
     justifyContent: 'center',
@@ -75,14 +33,4 @@ export const billboardContainer = style({
     gap: '2rem',
     padding: theme.dimensions.sectionPadding,
     border: theme.border
-})
-
-export const aboutContainer = style({
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'space-between',
-    position: 'relative',
-    padding: '64px 0',
-    borderBottom: `2px solid ${theme.colors.primary.no12}`,
-    backgroundColor: theme.colors.primary.no07
 })
