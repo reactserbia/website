c78711fcdf8076600e9c962fe9316abbcb5248ad
Mirko Basic <bejzik8@gmail.com>
2023-12-09 02:32:50 +0100

    Created Button component. 🍿

---
+++ src/comp/Button/Button.css.ts
@@ -0,0 +1,10 @@
+import { style } from '@vanilla-extract/css'
+
+import { theme } from '@/style/theme.css'
+
+export const button = style({
+    padding: '0.5rem 1.5rem',
+    border: `2px solid ${theme.colors.black}`,
+    borderRadius: 999,
+    backgroundColor: theme.colors.tertiary.no02
+})
---
+++ src/comp/Button/Button.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react'
+
+import { button } from './Button.css'
+
+type ButtonProps = {
+    children: ReactNode
+}
+
+export function Button({ children }: ButtonProps) {
+    return <button className={button}>{children}</button>
+}
