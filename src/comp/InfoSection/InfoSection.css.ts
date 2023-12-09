4c28c89182e37715d58d27d3fd41bf7279b26152
Mirko Basic <bejzik8@gmail.com>
2023-12-09 04:31:25 +0100

    Updated InfoSection. 🦏

--- src/comp/InfoSection/InfoSection.css.ts
+++ src/comp/InfoSection/InfoSection.css.ts
@@ -4,6 +4,23 @@ import { style } from '@vanilla-extract/css'
 export const container = style({
     width: '100%',
     maxWidth: '450px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    gap: '1rem',
     padding: '1.5rem',
-    border: theme.border
+    borderRadius: '1rem',
+    border: theme.border,
+    backgroundColor: theme.colors.tertiary.no02,
+    boxShadow: theme.boxShadow.large,
+    textAlign: 'center'
 })
+
+export const heading = style({
+    lineHeight: 1,
+    fontSize: '2rem',
+    margin: 0
+})
+
+export const paragraph = style({})
--- src/comp/InfoSection/InfoSection.tsx
+++ src/comp/InfoSection/InfoSection.tsx
@@ -1,5 +1,33 @@
+import Image from 'next/image'
+import localFont from 'next/font/local'
+
 import { container } from './InfoSection.css'
 
-export function InfoSection() {
-    return <div className={container}>IS</div>
+const komika = localFont({ src: '../../assets/fonts/KomikaHand.ttf' })
+const komikaBold = localFont({ src: '../../assets/fonts/KomikaHandBold.ttf' })
+
+type InfoSectionProps = {
+    heading: string
+    content: string
+}
+
+export function InfoSection({ heading, content }: InfoSectionProps) {
+    return (
+        <div className={container}>
+            <h1 className={`${komikaBold.className}`}>{heading}</h1>
+            <Image
+                width={104}
+                height={22}
+                src='/images/stars.svg'
+                alt='stars'
+            />
+            <p className={`${komika.className}`}>{content}</p>
+            <Image
+                width={104}
+                height={22}
+                src='/images/stars.svg'
+                alt='stars'
+            />
+        </div>
+    )
 }
