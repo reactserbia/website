02c4be2d7d7f20dc8a810265b6975ea98dde5b64
Mirko Basic <bejzik8@gmail.com>
2023-12-10 18:43:45 +0100

    Added details about the first meeting. 🚀

--- src/components/organisms/BillboardSection/BillboardSection.tsx
+++ src/components/organisms/BillboardSection/BillboardSection.tsx
@@ -1,4 +1,4 @@
-import { BoxedHeading, BoxedParagraph } from '@/components'
+import { BoxedHeading, BoxedParagraph, InfoSection } from '@/components'
 import { COLOR_VARIANTS, SHADE_VARIANTS, SIZE_VARIANTS } from '@/constants'
 import { billboardContainer } from './BillboardSection.css'
 
@@ -18,6 +18,23 @@ export function BillboardSection() {
                 things relevant to the work of the organisation and our
                 community.
             </BoxedParagraph>
+            <InfoSection heading='Meeting #1'>
+                We are planning to meet monthly and we are starting in January
+                of 2024! 🚀
+                <br />
+                <br />
+                The agenda for the first meeting is to clarify the ideology of
+                the organization, meet with the same-minded people, get to know
+                each other, maybe have some casual talks and definitely have a
+                good time sharing love and positivity.
+                <br />
+                <br />
+                We still do not have all the details figured out but we will
+                eventually and we will post them here. Stay tuned.
+                <br />
+                <br />
+                🍀
+            </InfoSection>
         </section>
     )
 }
