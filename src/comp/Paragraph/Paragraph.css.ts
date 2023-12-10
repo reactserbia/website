b8a464e2f6f8ca3b0f6481a65acfe390743484c0
Mirko Basic <bejzik8@gmail.com>
2023-12-10 02:10:57 +0100

    Updated Paragraph component. 🐨

--- src/comp/Paragraph/Paragraph.css.ts
+++ src/comp/Paragraph/Paragraph.css.ts
@@ -4,6 +4,6 @@ import { theme } from '@/style/theme.css'
 
 export const paragraph = style({
     lineHeight: 1,
-    fontSize: '1.25rem',
+    fontSize: '1rem',
     color: theme.colors.black
 })
--- src/comp/Paragraph/Paragraph.tsx
+++ src/comp/Paragraph/Paragraph.tsx
@@ -1,11 +1,14 @@
 import { ReactNode } from 'react'
+import localFont from 'next/font/local'
 
 import { paragraph } from './Paragraph.css'
+
+const komika = localFont({ src: '../../assets/fonts/KomikaHand.ttf' })
 
 type ParagraphProps = {
     children: ReactNode
 }
 
 export function Paragraph({ children }: ParagraphProps) {
-    return <p className={paragraph}>{children}</p>
+    return <p className={`${paragraph} ${komika.className}`}>{children}</p>
 }
