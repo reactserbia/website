da00b20fb790cc8f39a2334f92b525cc16a07771
Mirko Basic <bejzik8@gmail.com>
2023-12-10 05:11:37 +0100

    Removed unused stuff. ⛔️

--- src/components/atoms/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './Button/Button'
-export * from './Logo/Logo'
-export * from './NavigationButton/NavigationButton'
-export * from './Subheading/Subheading'
--- src/components/molecules/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './BoxedSubheading/BoxedSubheading'
-export * from './InfoSection/InfoSection'
-export * from './NoteEntry/NoteEntry'
