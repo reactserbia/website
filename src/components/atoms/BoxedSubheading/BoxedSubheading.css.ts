fce777bc150ff7ed4689a8016731fc06351734dd
Mirko Basic <bejzik8@gmail.com>
2023-12-11 18:47:43 +0100

    Updated BannerSection. 🌇

--- src/components/atoms/BoxedSubheading/BoxedSubheading.css.ts
+++ src/components/atoms/BoxedSubheading/BoxedSubheading.css.ts
@@ -9,6 +9,7 @@ export const subheading = style({
     lineHeight: 1,
     fontSize: '1.25rem',
     fontWeight: 700,
+    textAlign: 'center',
     color: theme.colors.black,
     backgroundColor: theme.colors.tertiary.no02,
     boxShadow: theme.boxShadow.small
