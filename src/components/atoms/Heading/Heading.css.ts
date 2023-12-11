3653d933bbb9e25d473cfd0bd97c2e7edb4f4dab
Mirko Basic <bejzik8@gmail.com>
2023-12-11 23:33:48 +0100

    Updated Heading component. 🐯

--- src/components/atoms/Heading/Heading.css.ts
+++ src/components/atoms/Heading/Heading.css.ts
@@ -2,163 +2,115 @@ import { style, styleVariants } from '@vanilla-extract/css'
 
 import { theme } from '@/style/theme.css'
 
-import {
-    COLOR_VARIANTS,
-    LAYERS_VARIANTS,
-    SHADE_VARIANTS,
-    SIZE_VARIANTS
-} from '@/constants'
+import { COLOR_VARIANTS, SHADE_VARIANTS } from '@/constants'
 
-export const base = style({
+const base = style({
     display: 'inline-block',
     position: 'relative',
     zIndex: 3,
     lineHeight: 1,
+    fontSize: '2rem',
     fontWeight: 900,
     WebkitTextStrokeWidth: '1px',
-    margin: 0
-})
+    margin: 0,
 
-export const sizeVariants = styleVariants({
-    [SIZE_VARIANTS.SMALL]: {
-        fontSize: '2rem'
-    },
-    [SIZE_VARIANTS.MEDIUM]: {
-        fontSize: '3rem'
-    },
-    [SIZE_VARIANTS.LARGE]: {
-        fontSize: '4rem'
+    '@media': {
+        'screen and (min-width: 576px)': {
+            fontSize: '3rem'
+        },
+        'screen and (min-width: 1024px)': {
+            fontSize: '4rem'
+        }
     }
 })
 
 export const textColorVariants = styleVariants({
-    [`${COLOR_VARIANTS.PRIMARY}-${SHADE_VARIANTS.LIGHT}`]: {
-        color: theme.colors.primary.no03
-    },
-    [`${COLOR_VARIANTS.PRIMARY}-${SHADE_VARIANTS.DARK}`]: {
-        color: theme.colors.primary.no06
-    },
-    [`${COLOR_VARIANTS.SECONDARY}-${SHADE_VARIANTS.LIGHT}`]: {
-        color: theme.colors.secondary.no03
-    },
-    [`${COLOR_VARIANTS.SECONDARY}-${SHADE_VARIANTS.DARK}`]: {
-        color: theme.colors.secondary.no06
-    },
-    [`${COLOR_VARIANTS.TERTIARY}-${SHADE_VARIANTS.LIGHT}`]: {
-        color: theme.colors.tertiary.no03
-    },
-    [`${COLOR_VARIANTS.TERTIARY}-${SHADE_VARIANTS.DARK}`]: {
-        color: theme.colors.tertiary.no06
-    }
-})
-
-export const strokeColorVariants = styleVariants({
-    [`${COLOR_VARIANTS.PRIMARY}-${LAYERS_VARIANTS.TWO}`]: {
-        WebkitTextStrokeColor: theme.colors.primary.no12
-    },
-    [`${COLOR_VARIANTS.PRIMARY}-${LAYERS_VARIANTS.THREE}`]: {
-        WebkitTextStrokeColor: theme.colors.primary.no11
-    },
-    [`${COLOR_VARIANTS.SECONDARY}-${LAYERS_VARIANTS.TWO}`]: {
-        WebkitTextStrokeColor: theme.colors.secondary.no12
-    },
-    [`${COLOR_VARIANTS.SECONDARY}-${LAYERS_VARIANTS.THREE}`]: {
-        WebkitTextStrokeColor: theme.colors.secondary.no11
-    },
-    [`${COLOR_VARIANTS.TERTIARY}-${LAYERS_VARIANTS.TWO}`]: {
-        WebkitTextStrokeColor: theme.colors.tertiary.no12
-    },
-    [`${COLOR_VARIANTS.TERTIARY}-${LAYERS_VARIANTS.THREE}`]: {
-        WebkitTextStrokeColor: theme.colors.tertiary.no11
-    }
-})
-
-export const spanBase = style({
-    position: 'absolute',
-    whiteSpace: 'nowrap',
-    userSelect: 'none'
-})
-
-export const spanOneColorVariants = styleVariants({
-    [COLOR_VARIANTS.PRIMARY]: {
-        zIndex: -1,
-        color: theme.colors.primary.no11,
-        WebkitTextStrokeColor: theme.colors.primary.no11
-    },
-    [COLOR_VARIANTS.SECONDARY]: {
-        zIndex: -1,
-        color: theme.colors.secondary.no11,
-        WebkitTextStrokeColor: theme.colors.secondary.no11
-    },
-    [COLOR_VARIANTS.TERTIARY]: {
-        zIndex: -1,
-        color: theme.colors.tertiary.no11,
-        WebkitTextStrokeColor: theme.colors.tertiary.no11
-    }
-})
-
-export const spanTwoColorVariants = styleVariants({
-    [COLOR_VARIANTS.PRIMARY]: {
-        zIndex: -2,
-        color: theme.colors.primary.no12,
-        WebkitTextStrokeColor: theme.colors.primary.no12
-    },
-    [COLOR_VARIANTS.SECONDARY]: {
-        zIndex: -2,
-        color: theme.colors.secondary.no12,
-        WebkitTextStrokeColor: theme.colors.secondary.no12
-    },
-    [COLOR_VARIANTS.TERTIARY]: {
-        zIndex: -2,
-        color: theme.colors.tertiary.no12,
-        WebkitTextStrokeColor: theme.colors.tertiary.no12
-    }
-})
-
-export const spanOneSizeVariants = styleVariants({
-    [SIZE_VARIANTS.SMALL]: [
-        spanBase,
+    [`${COLOR_VARIANTS.PRIMARY}-${SHADE_VARIANTS.LIGHT}`]: [
+        base,
         {
-            left: 3,
-            top: 3
+            color: theme.colors.primary.no03,
+            WebkitTextStrokeColor: theme.colors.primary.no12
         }
     ],
-    [SIZE_VARIANTS.MEDIUM]: [
-        spanBase,
+    [`${COLOR_VARIANTS.PRIMARY}-${SHADE_VARIANTS.DARK}`]: [
+        base,
         {
-            left: 4,
-            top: 4
+            color: theme.colors.primary.no06,
+            WebkitTextStrokeColor: theme.colors.primary.no12
         }
     ],
-    [SIZE_VARIANTS.LARGE]: [
-        spanBase,
+    [`${COLOR_VARIANTS.SECONDARY}-${SHADE_VARIANTS.LIGHT}`]: [
+        base,
         {
-            left: 5,
-            top: 5
+            color: theme.colors.secondary.no03,
+            WebkitTextStrokeColor: theme.colors.secondary.no12
+        }
+    ],
+    [`${COLOR_VARIANTS.SECONDARY}-${SHADE_VARIANTS.DARK}`]: [
+        base,
+        {
+            color: theme.colors.secondary.no06,
+            WebkitTextStrokeColor: theme.colors.secondary.no12
+        }
+    ],
+    [`${COLOR_VARIANTS.TERTIARY}-${SHADE_VARIANTS.LIGHT}`]: [
+        base,
+        {
+            color: theme.colors.tertiary.no03,
+            WebkitTextStrokeColor: theme.colors.tertiary.no12
+        }
+    ],
+    [`${COLOR_VARIANTS.TERTIARY}-${SHADE_VARIANTS.DARK}`]: [
+        base,
+        {
+            color: theme.colors.tertiary.no06,
+            WebkitTextStrokeColor: theme.colors.tertiary.no12
         }
     ]
 })
 
-export const spanTwoSizeVariants = styleVariants({
-    [SIZE_VARIANTS.SMALL]: [
+const spanBase = style({
+    position: 'absolute',
+    left: 3,
+    top: 3,
+    whiteSpace: 'nowrap',
+    userSelect: 'none',
+
+    '@media': {
+        'screen and (min-width: 576px)': {
+            left: 4,
+            top: 4
+        },
+        'screen and (min-width: 1024px)': {
+            left: 5,
+            top: 5
+        }
+    }
+})
+
+export const spanColorVariants = styleVariants({
+    [COLOR_VARIANTS.PRIMARY]: [
         spanBase,
         {
-            left: 6,
-            top: 6
+            zIndex: -1,
+            color: theme.colors.primary.no12,
+            WebkitTextStrokeColor: theme.colors.primary.no12
         }
     ],
-    [SIZE_VARIANTS.MEDIUM]: [
+    [COLOR_VARIANTS.SECONDARY]: [
         spanBase,
         {
-            left: 8,
-            top: 8
+            zIndex: -1,
+            color: theme.colors.secondary.no12,
+            WebkitTextStrokeColor: theme.colors.secondary.no12
         }
     ],
-    [SIZE_VARIANTS.LARGE]: [
+    [COLOR_VARIANTS.TERTIARY]: [
         spanBase,
         {
-            left: 10,
-            top: 10
+            zIndex: -1,
+            color: theme.colors.tertiary.no12,
+            WebkitTextStrokeColor: theme.colors.tertiary.no12
         }
     ]
 })
--- src/components/atoms/Heading/Heading.tsx
+++ src/components/atoms/Heading/Heading.tsx
@@ -1,66 +1,31 @@
 import { ReactNode } from 'react'
 import { Rubik } from 'next/font/google'
 
-import {
-    COLOR_VARIANTS,
-    LAYERS_VARIANTS,
-    SHADE_VARIANTS,
-    SIZE_VARIANTS
-} from '@/constants'
+import { COLOR_VARIANTS, SHADE_VARIANTS } from '@/constants'
 
-import {
-    base,
-    spanOneSizeVariants,
-    spanTwoSizeVariants,
-    textColorVariants,
-    sizeVariants,
-    strokeColorVariants,
-    spanOneColorVariants,
-    spanTwoColorVariants
-} from './Heading.css'
+import { textColorVariants, spanColorVariants } from './Heading.css'
 
 const rubik = Rubik({ subsets: ['latin'] })
 
 export type HeadingProps = {
     children: ReactNode
     colorVariant?: COLOR_VARIANTS
-    sizeVariant?: SIZE_VARIANTS
-    layers?: LAYERS_VARIANTS
     shadeVariant?: SHADE_VARIANTS
 }
 
 export function Heading({
     children,
     colorVariant = COLOR_VARIANTS.PRIMARY,
-    sizeVariant = SIZE_VARIANTS.SMALL,
-    layers = LAYERS_VARIANTS.TWO,
     shadeVariant = SHADE_VARIANTS.LIGHT
 }: HeadingProps) {
     return (
         <h1
-            className={`${rubik.className} ${base} ${
+            className={`${rubik.className} ${
                 textColorVariants[`${colorVariant}-${shadeVariant}`]
-            } ${sizeVariants[sizeVariant]} ${
-                strokeColorVariants[`${colorVariant}-${layers}`]
             }`}
         >
             {children}
-            <span
-                className={`${spanOneSizeVariants[sizeVariant]} ${
-                    (layers === LAYERS_VARIANTS.TWO
-                        ? spanTwoColorVariants
-                        : spanOneColorVariants)[colorVariant]
-                }`}
-            >
-                {children}
-            </span>
-            {layers >= LAYERS_VARIANTS.THREE && (
-                <span
-                    className={`${spanTwoSizeVariants[sizeVariant]} ${spanTwoColorVariants[colorVariant]}`}
-                >
-                    {children}
-                </span>
-            )}
+            <span className={spanColorVariants[colorVariant]}>{children}</span>
         </h1>
     )
 }
