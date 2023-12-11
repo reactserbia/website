a7c726312d4ceca8892e394040930c38d60fb8b5
Mirko Basic <bejzik8@gmail.com>
2023-12-11 19:38:21 +0100

    Updated BoxedSubheading component. 🔊

--- src/components/atoms/BoxedSubheading/BoxedSubheading.css.ts
+++ src/components/atoms/BoxedSubheading/BoxedSubheading.css.ts
@@ -4,13 +4,20 @@ import { theme } from '@/style/theme.css'
 
 export const subheading = style({
     padding: '0rem 1.5rem 0.25rem',
-    border: theme.border,
+    border: `2px solid ${theme.colors.black}`,
     borderRadius: 999,
     lineHeight: 1,
-    fontSize: '1.25rem',
+    fontSize: '1rem',
     fontWeight: 700,
     textAlign: 'center',
     color: theme.colors.black,
     backgroundColor: theme.colors.tertiary.no02,
-    boxShadow: theme.boxShadow.small
+    boxShadow: theme.boxShadow.small,
+
+    '@media': {
+        'screen and (min-width: 576px)': {
+            border: theme.border,
+            fontSize: '1.25rem'
+        }
+    }
 })
