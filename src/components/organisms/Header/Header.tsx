34a204bff166ad5ef351d41fb5714cf53c3a0a29
Mirko Basic <bejzik8@gmail.com>
2023-12-12 05:25:28 +0100

    Updated Header component. 🦌

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,15 +1,10 @@
-import { COLOR_VARIANTS } from '@/constants'
-import { Button, Heading, Logo } from '@/components/atoms'
+import { Button, Logo } from '@/components/atoms'
 import { header, navigation } from './Header.css'
 
 export function Header() {
     return (
         <header className={header}>
             <Logo />
-            {/* <div>
-                <Heading colorVariant={COLOR_VARIANTS.TERTIARY}>REACT</Heading>
-                <Heading>SERBIA</Heading>
-            </div> */}
             <nav className={navigation}>
                 <Button>Home</Button>
                 <Button>Ideology</Button>
