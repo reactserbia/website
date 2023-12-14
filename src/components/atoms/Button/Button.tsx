8768f94da4ab7995edad232c1174e07490a37f50
Mirko Basic <bejzik8@gmail.com>
2023-12-14 04:58:18 +0100

    Updated Button component. 🧈

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,11 +1,17 @@
 import { ReactNode } from 'react'
+import Link from 'next/link'
 
 import { button } from './Button.css'
 
 type ButtonProps = {
     children: ReactNode
+    href: string
 }
 
-export function Button({ children }: ButtonProps) {
-    return <button className={button}>{children}</button>
+export function Button({ children, href }: ButtonProps) {
+    return (
+        <Link href={href} className={button}>
+            {children}
+        </Link>
+    )
 }
