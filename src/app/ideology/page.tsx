f0c8915685ced05763a4f22307ee92e04f32dd7a
Mirko Basic <bejzik8@gmail.com>
2023-12-21 13:07:21 +0100

    Started working on ideology page. 🍀

--- src/app/ideology/page.tsx
+++ src/app/ideology/page.tsx
@@ -1,10 +1,29 @@
-import { Page } from '@/components'
+import { BoxedHeading, BoxedParagraph, Page } from '@/components'
+import { COLOR_VARIANTS, SHADE_VARIANTS } from '@/constants'
 import { NAVIGATION_LINK_TYPE } from '@/models'
+
+import { sectionContainer } from './page.css'
 
 export default function Ideology() {
     return (
         <Page page={NAVIGATION_LINK_TYPE.IDEOLOGY}>
-            <h1>Ideology Page</h1>
+            <section className={sectionContainer}>
+                <BoxedHeading
+                    colorVariant={COLOR_VARIANTS.TERTIARY}
+                    shadeVariant={SHADE_VARIANTS.DARK}
+                >
+                    IDEOLOGY
+                </BoxedHeading>
+                <BoxedParagraph>
+                    On this page, you will find the ideology behind the
+                    organization. This is the place where you should be able to
+                    dive into our minds, see how we think, why are we doing
+                    things certain way, what are our goals, how we operate, etc.
+                    Based on all that, you should be able to draw the conclution
+                    about the organization, see if you like the idea and whether
+                    you want to be a part of it or not. We hope you do! 🍀
+                </BoxedParagraph>
+            </section>
         </Page>
     )
 }
