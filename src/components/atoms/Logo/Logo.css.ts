b7cfa09d42c1fea0b974022233e64629680cc97f
Mirko Basic <bejzik8@gmail.com>
2023-12-24 20:33:46 +0100

    Updated Logo component. 🐙

--- src/components/atoms/Logo/Logo.css.ts
+++ src/components/atoms/Logo/Logo.css.ts
@@ -1,10 +1,9 @@
-import { style } from '@vanilla-extract/css'
+import { style, styleVariants } from '@vanilla-extract/css'
 
+import { SIZE_VARIANTS } from '@/constants'
 import { theme } from '@/style'
 
-export const container = style({
-    width: '3rem',
-    height: '3rem',
+export const containerBase = style({
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
@@ -14,7 +13,46 @@ export const container = style({
     boxShadow: theme.boxShadow.small
 })
 
-export const logoImage = style({
+export const container = styleVariants({
+    [SIZE_VARIANTS.LARGE]: [
+        containerBase,
+        {
+            width: '3rem',
+            height: '3rem'
+        }
+    ],
+    [SIZE_VARIANTS.MEDIUM]: [
+        containerBase,
+        {
+            width: '2.5rem',
+            height: '2.5rem'
+        }
+    ],
+    [SIZE_VARIANTS.SMALL]: [
+        containerBase,
+        {
+            width: '2rem',
+            height: '2rem'
+        }
+    ]
+})
+
+export const logoImageBase = style({
     width: '2rem',
     height: '2rem'
+})
+
+export const logoImage = styleVariants({
+    [SIZE_VARIANTS.LARGE]: {
+        width: '2rem',
+        height: '2rem'
+    },
+    [SIZE_VARIANTS.MEDIUM]: {
+        width: '1.6rem',
+        height: '1.6rem'
+    },
+    [SIZE_VARIANTS.SMALL]: {
+        width: '1.2rem',
+        height: '1.2rem'
+    }
 })
