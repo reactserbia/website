83946bd685788d115cefdd5d5a155a666709ec9c
Mirko Basic <bejzik8@gmail.com>
2023-12-24 19:56:08 +0100

    Updated EmailSubscribe layout. 🐬

--- src/components/atoms/Input/Input.css.ts
+++ src/components/atoms/Input/Input.css.ts
@@ -4,7 +4,6 @@ import { theme } from '@/style'
 
 export const input = style({
     width: '100%',
-    maxWidth: '300px',
     padding: '0.5rem 1rem',
     border: theme.border.small,
     borderRadius: 999,
