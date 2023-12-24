acb55c22a139aca529547c8b4cb50caa8b2945e8
Mirko Basic <bejzik8@gmail.com>
2023-12-24 18:02:34 +0100

    Updated socialLinks constant. 🔗

--- src/constants/socialNetworks.ts
+++ src/constants/socialNetworks.ts
@@ -1,6 +1,6 @@
 import { SOCIAL_NETWORK_ICONS, SocialLinkType } from '@/models'
 
-export const socialLinks: SocialLinkType[] = [
+export const socialLinksReactSerbia: SocialLinkType[] = [
     {
         type: SOCIAL_NETWORK_ICONS.GITHUB,
         url: 'https://github.com/ReactSerbia'
@@ -12,5 +12,28 @@ export const socialLinks: SocialLinkType[] = [
     {
         type: SOCIAL_NETWORK_ICONS.TELEGRAM,
         url: 'https://t.me/+puv-aR71sU1iNTlk'
+    }
+]
+
+export const socialLinksMirko: SocialLinkType[] = [
+    {
+        type: SOCIAL_NETWORK_ICONS.GITHUB,
+        url: 'https://github.com/bejzik8'
+    },
+    {
+        type: SOCIAL_NETWORK_ICONS.TWITTER,
+        url: 'https://twitter.com/bejzik8'
+    },
+    {
+        type: SOCIAL_NETWORK_ICONS.TELEGRAM,
+        url: 'https://t.me/bejzik'
+    },
+    {
+        type: SOCIAL_NETWORK_ICONS.LINKEDIN,
+        url: 'https://www.linkedin.com/in/mirkobasic'
+    },
+    {
+        type: SOCIAL_NETWORK_ICONS.INSTAGRAM,
+        url: 'https://www.instagram.com/b8zeek'
     }
 ]
