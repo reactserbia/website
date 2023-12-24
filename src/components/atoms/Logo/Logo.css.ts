0a12730e0a1f63ba800d635a1b486a07338372f0
Mirko Basic <bejzik8@gmail.com>
2023-12-24 20:33:46 +0100

    Updated Logo component. 🐙

--- src/components/atoms/Logo/Logo.css.ts
+++ src/components/atoms/Logo/Logo.css.ts
@@ -1,11 +1,10 @@
-import { style } from '@vanilla-extract/css'
+import { style, styleVariants } from '@vanilla-extract/css'
 
+import { SIZE_VARIANTS } from '@/constants'
 import { theme } from '@/style'
 import { flexCenter } from '@/style/utils'
 
-export const container = style({
-    width: '3rem',
-    height: '3rem',
+export const containerBase = style({
     ...flexCenter,
     border: theme.border.small,
     borderRadius: 999,
@@ -13,7 +12,46 @@ export const container = style({
     boxShadow: theme.boxShadow.small
 })
 
-export const logoImage = style({
+export const container = styleVariants({
+    [SIZE_VARIANTS.LARGE]: [
+        containerBase,
+        {
+            width: '3rem',
+            height: '3rem'
+        }
+    ],
+    [SIZE_VARIANTS.MEDIUM]: [
+        containerBase,
+        {
+            width: '2.5rem',
+            height: '2.5rem'
+        }
+    ],
+    [SIZE_VARIANTS.SMALL]: [
+        containerBase,
+        {
+            width: '2rem',
+            height: '2rem'
+        }
+    ]
+})
+
+export const logoImageBase = style({
     width: '2rem',
     height: '2rem'
+})
+
+export const logoImage = styleVariants({
+    [SIZE_VARIANTS.LARGE]: {
+        width: '2rem',
+        height: '2rem'
+    },
+    [SIZE_VARIANTS.MEDIUM]: {
+        width: '1.6rem',
+        height: '1.6rem'
+    },
+    [SIZE_VARIANTS.SMALL]: {
+        width: '1.2rem',
+        height: '1.2rem'
+    }
 })
--- src/components/atoms/Logo/Logo.tsx
+++ src/components/atoms/Logo/Logo.tsx
@@ -1,16 +1,22 @@
 import Image from 'next/image'
 
+import { SIZE_VARIANTS } from '@/constants'
+
 import { container, logoImage } from './Logo.css'
 
-export function Logo() {
+type LogoProps = {
+    sizeVariant?: SIZE_VARIANTS
+}
+
+export function Logo({ sizeVariant = SIZE_VARIANTS.LARGE }: LogoProps) {
     return (
-        <div className={container}>
+        <div className={container[sizeVariant]}>
             <Image
                 width={46}
                 height={46}
                 src='/images/logo.svg'
                 alt='logo'
-                className={logoImage}
+                className={logoImage[sizeVariant]}
             />
         </div>
     )
