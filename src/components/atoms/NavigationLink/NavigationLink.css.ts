a2f2a9d13e743330bf1fad374c5e5b1bb3fba747
Mirko Basic <bejzik8@gmail.com>
2023-12-24 18:54:04 +0100

    Created NavigationLink component. 🧭

---
+++ src/components/atoms/NavigationLink/NavigationLink.css.ts
@@ -0,0 +1,24 @@
+import { style } from '@vanilla-extract/css'
+
+import { theme } from '@/style'
+
+export const navigationLink = style({
+    padding: '0.5rem 1.25rem',
+    border: `2px solid ${theme.colors.black}`,
+    borderRadius: 999,
+    backgroundColor: theme.colors.tertiary.no02,
+    lineHeight: 1,
+    fontSize: '1rem',
+    fontFamily: 'inherit',
+    fontWeight: 700,
+    color: theme.colors.black,
+    textDecoration: 'none',
+    boxShadow: theme.boxShadow.small,
+    cursor: 'pointer',
+
+    selectors: {
+        '&:hover': {
+            backgroundColor: theme.colors.tertiary.no03
+        }
+    }
+})
