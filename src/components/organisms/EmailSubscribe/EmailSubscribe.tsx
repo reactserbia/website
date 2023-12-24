2e87a66224d27b110625a5e4baf1e08f7f8c95a5
Mirko Basic <bejzik8@gmail.com>
2023-12-24 21:50:36 +0100

    Updated EmailSubscribe component. 🦁

--- src/components/organisms/EmailSubscribe/EmailSubscribe.tsx
+++ src/components/organisms/EmailSubscribe/EmailSubscribe.tsx
@@ -3,11 +3,14 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import toast from 'react-hot-toast'
+import { z } from 'zod'
 
 import { Button, Input } from '@/components'
 import { emailSubscribeSchema } from '@/models'
 
 import { container } from './EmailSubscribe.css'
+
+type EmailSubscriber = z.infer<typeof emailSubscribeSchema>
 
 export function EmailSubscribe() {
     const {
@@ -16,12 +19,25 @@ export function EmailSubscribe() {
         formState: { isValid }
     } = useForm({ resolver: zodResolver(emailSubscribeSchema) })
 
-    const onClickHandler = () => toast.error('Implementation in progress...')
+    const onClick = async (data: EmailSubscriber) => {
+        const response = await fetch('/api/subscribe', {
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+
+        const resdata = await response.json()
+
+        if (!response.ok) {
+            toast.error(resdata.message)
+        } else {
+            toast.success(resdata.message)
+        }
+    }
 
     return (
         <form
             className={container}
-            onSubmit={handleSubmit(d => console.log(d))}
+            onSubmit={handleSubmit(data => onClick(data as EmailSubscriber))}
         >
             <Input
                 placeholder='First Name'
@@ -34,12 +50,7 @@ export function EmailSubscribe() {
                 register={register}
             />
             <Input placeholder='Email' name='email' register={register} />
-            <Button
-                type='submit'
-                disabled={!isValid}
-                marginTop='1rem'
-                onClick={onClickHandler}
-            >
+            <Button type='submit' disabled={!isValid} marginTop='1rem'>
                 Subscribe to Email
             </Button>
         </form>
